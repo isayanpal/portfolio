244c9a3d4971b53d61b0c670fc2ea475d70f4f4b
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-08-06 04:40:28 +0530

    fix: next image

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { socialMedia } from "@/data";
 import { IoIosMail } from "react-icons/io";
+import Image from "next/image";
 
 const Connect = () => {
   return (
-    <footer className="w-full  pb-10" id="connect">
-      {/* background grid */}
-
+    <footer className="w-full pb-10" id="connect">
       <div className="flex flex-col items-center">
         <h1 className="heading text-[45px] lg:max-w-[45vw]">
-          And <span className="text-purple">Thats</span> all!
+          And <span className="text-purple">That's</span> all!
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Eager to collaborate and create impactful digital experiences? Reach
-          out, and let&apos;s turn ideas into reality!
+          out, and let's turn ideas into reality!
         </p>
         <a href="mailto:sayanpalb@gmail.com">
           <MagicButton
@@ -34,11 +33,12 @@ const Connect = () => {
               rel="noopener noreferrer"
               className="w-10 h-10 flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img
+              <Image
                 src={info.img}
                 alt="Social Media Icon"
                 width={20}
                 height={20}
+                className="w-5 h-5"
               />
             </a>
           ))}
